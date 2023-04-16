ddf87f0fb74c3a4914349ec3757bf25b29363671
vilitaslay <jvilabach@mail.utdt.edu>
2023-04-16 15:13:50 -0300

    base de datos

--- scripts/chart_01.js
+++ scripts/chart_01.js
@@ -1,4 +1,4 @@
-d3.csv('data/147_desratizacion_nuevo.csv.csv', d3.autoType).then(data => {
+d3.csv('147_desratizacion_nuevo.csv', d3.autoType).then(data => {
     console.log(data)
 
     let chart = Plot.plot({
